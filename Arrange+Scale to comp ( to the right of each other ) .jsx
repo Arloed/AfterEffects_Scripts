8b090e35098f70cb6350f706a34b7987d75c2570
Arloed <arloedmail@gmail.com>
2024-12-26 13:10:44 -0500

    Add files via upload
    This aftereffect script will help you to arrange multiple images to the right side or the left side of each other (if you want to make a slideshow effect) also it automatically resizes any image to the composition dimensions

---
+++ Arrange+Scale to comp ( to the right of each other ) .jsx
@@ -0,0 +1,33 @@
+var comp = app.project.activeItem; // Get the active composition
+
+if (comp && comp.selectedLayers.length > 0) {
+    var compHeight = comp.height; // Composition height
+    var currentX = 0; // Starting X position for the first image
+
+    for (var i = 0; i < comp.selectedLayers.length; i++) {
+        var layer = comp.selectedLayers[i];
+        
+        // Get the layer's source dimensions
+        var bounds = layer.sourceRectAtTime(0, false);
+        var layerWidth = bounds.width;
+        var layerHeight = bounds.height;
+
+        // Calculate the scale to fit the composition height
+        var scaleFactor = (compHeight / layerHeight) * 100;
+        layer.transform.scale.setValue([scaleFactor, scaleFactor]); // Uniform scaling
+
+        // Update the layer's width after scaling
+        var scaledWidth = (layerWidth * scaleFactor) / 100;
+
+        // Align the layer next to the previous one
+        layer.position.setValue([
+            currentX + scaledWidth / 2 - bounds.left,
+            compHeight / 2
+        ]);
+
+        // Update the current X position for the next layer
+        currentX += scaledWidth;
+    }
+} else {
+    alert("Please select the layers you want to arrange side by side.");
+}
